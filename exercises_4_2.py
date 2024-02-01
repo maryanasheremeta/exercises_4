aec1c6144a1cb9ae21fafa5f357eb6424dc00bbb
maryanasheremeta <maryanasc96@gmail.com>
2024-02-01 18:29:29 +0100

    esercizi numero 2

--- exercises_4_2.py
+++ exercises_4_2.py
@@ -3,5 +3,15 @@ my_numbers = numbers[0:5:2]
 my_new_numbers = sum(my_numbers)*numbers[-1]
 print(my_new_numbers)
 
+my_list=[0,1,7,2,4,8]
+result=0
+for i in range(0,len(my_list),2):
+    result+=my_list[i]
+
+if my_list!=[]:
+    result=result*my_list[-1]
+    print(result)
+
+
 
 
