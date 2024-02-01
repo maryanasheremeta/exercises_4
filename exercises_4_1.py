6708c8fc96385b6fa81d18aac4cd95ad10c2d5bf
maryanasheremeta <maryanasc96@gmail.com>
2024-02-01 18:24:54 +0100

    esercizi numero 2

--- exercises_4_1.py
+++ exercises_4_1.py
@@ -1,7 +1,14 @@
-my_numbers = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-my_numbers.sort(key=bool, reverse=True)
-print(my_numbers)
+#my_numbers = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+#my_numbers.sort(key=bool, reverse=True)
+#print(my_numbers)
 
+my_list=[9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96,]
+
+#for index in range(len(my_list)):
+    if my_list[index]==0:
+         my_list.remove(0)
+         my_list.append(0)
+         print(my_list)
 
 
 
